f9425fab8313d4285a1d44d97115f79f3fecd60f
lukis76 <kiu.manoguerra@gmail.com>
2022-10-16 17:53:59 -0300

    home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { useSession } from 'next-auth/react'
 import UserNameForm from '../components/userNameForm'
 import useUserInfo from '../hooks/getUserInfo'
-
+//
 const Home = () => {
   /*--------------------------------------------- */
   const { userInfo, status } = useUserInfo()
@@ -9,14 +8,34 @@ const Home = () => {
   if (status === 'loading') {
     return 'loading user info'
   }
-  if (!userInfo.username) {
+  ///////////////////////////////
+  if (!userInfo.user.username) {
     return <UserNameForm />
   }
   /*--------------------------------------------- */
   /*--------------------------------------------- */
   return (
-    <div>
-      <h1>testing Home</h1>
+    <div className='max-w-xl mx-auto border-l border-r border-slate-700 min-h-screen'>
+      <h1 className='text-xl font-bold text-white p-4'>Home</h1>
+      <form className='mx-5'>
+        <div className='flex'>
+          <div>
+            <div className='rounded-full overflow-hidden w-12'>
+              <img src={userInfo.user.image} alt='avatar' />
+            </div>
+          </div>
+          <div className='grow pl-4'>
+            <textarea
+              className='w-full p-2 bg-transparent text-yellow-50'
+              placeholder={"What's happening?"}
+            />
+            <div className='text-right'>
+
+            <button className='bg-blue-600 text-white px-5 py-0.5 rounded-full font-semibold' >hiss</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   )
   /*--------------------------------------------- */
