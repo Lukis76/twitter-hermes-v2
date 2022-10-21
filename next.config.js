04fe5bd14faf30b99439f40370f55d1be19b7c4d
lukis76 <kiu.manoguerra@gmail.com>
2022-10-21 16:40:13 -0300

    img

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
